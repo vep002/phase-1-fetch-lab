576159118e04e50832af80f98921f2310ecb0127
Victoria <vep002@gmail.com>
2021-05-05 09:15:58 -0500

    commit tree feb558580e2101862ef5af9a98061e4e683ed14c

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch ('https://anapioficeandfire.com/api/books')
+  .then  (resp => resp.json())
+  .then (resp => renderBooks(resp))
 }
+// fetch('http://localhost:3000/pokemon',{
+//   method: 'POST',
+//   headers:{
+//       'Content-Type':'application/json'
+//   },
+//   body: JSON.stringify(pokemon)
+// })
+// .then(res => res.json())
+// .then(pokemon =>{
+//   renderPokemon(pokemon)
+// } )
+// }
+// function getAllPokemon(){
+//   fetch('http://localhost:3000/pokemon')
+//   .then(res => res.json())
+//   .then(pokemon => pokemon.forEach(pok => renderPokemon(pok)))
+// }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
